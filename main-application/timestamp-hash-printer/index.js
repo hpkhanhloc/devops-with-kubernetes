8903b5a78a4bb02386148a8eb09df6bb40dada22
Loc Hoang <hpkhanhloc@gmail.com>
2021-01-08 16:52:38 +0200

    part 2: exercise 1.01

--- main-application/timestamp-hash-printer/index.js
+++ main-application/timestamp-hash-printer/index.js
@@ -1,31 +1,30 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const axios = require("axios");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 const directory = path.join("/", "usr", "src", "app", "files");
 const textFilePath = path.join(directory, "text.txt");
-const pingpongFilePath = path.join(directory, "pingpong.txt");
 
 const createString = () => {
   return Math.random().toString(36).substr(2, 10);
 };
 const randomString = createString();
 
-app.get("/", (req, res) => {
-  fs.readFile(textFilePath, "utf8", (err, text) => {
+app.get("/", async (req, res) => {
+  let pingpong = "";
+  let text = "";
+  fs.readFile(textFilePath, "utf8", (err, data) => {
     if (err) {
       res.json(err);
     }
-    fs.readFile(pingpongFilePath, "utf8", (err, counter) => {
-      if (err) {
-        res.json(err);
-      }
-      res.json(`${text}: ${randomString}, Ping / Pong: ${counter}`);
-    });
+    text = data;
   });
+  await axios.get("/pingpong").then((res) => (pingpong = res.data));
+  res.json(`${text}: ${randomString}, ${pingpong}`);
 });
 
 app.listen(PORT, () => {
