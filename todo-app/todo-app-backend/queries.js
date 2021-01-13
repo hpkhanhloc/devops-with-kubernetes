f8ca166ddc04ee6d6bfa27e5c95cb99dc729afc2
Loc Hoang <hpkhanhloc@gmail.com>
2021-01-13 22:16:34 +0200

    part2: exercise 2.10

--- todo-app/todo-app-backend/queries.js
+++ todo-app/todo-app-backend/queries.js
@@ -3,6 +3,7 @@ const connectionString = `postgres://postgres:${process.env.POSTGRES_PASSWORD}@p
 const pool = new Pool({ connectionString: connectionString });
 
 const getTodos = async (request, response) => {
+  console.log("Get all todos...");
   await pool
     .query("SELECT * FROM todos")
     .then((result) => response.status(200).json(result.rows))
@@ -11,10 +12,15 @@ const getTodos = async (request, response) => {
 
 const postTodo = async (request, response) => {
   const { todo } = request.body;
-  await pool
-    .query(`INSERT INTO todos (to_do) VALUES ($1) RETURNING *`, [`${todo}`])
-    .then((result) => response.status(201).json(result.rows[0]))
-    .catch((e) => console.error(e.stack));
+  console.log(`Inserting new todo: ${todo}`);
+  if (todo.length > 140) {
+    response.status(422).json("Maximum leng is 140");
+  } else {
+    await pool
+      .query(`INSERT INTO todos (to_do) VALUES ($1) RETURNING *`, [`${todo}`])
+      .then((result) => response.status(201).json(result.rows[0]))
+      .catch((e) => console.error(e.stack));
+  }
 };
 
 module.exports = { getTodos, postTodo };
